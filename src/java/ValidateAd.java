f6e1ba46aad447276f77a37e40c320b2f2b55911
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-04-07 17:41:16 +0100

    Eleventh Commit: Implement ValidateAd.java, Update total.css and Alogin.html

--- src/java/ValidateAd.java
+++ src/java/ValidateAd.java
@@ -3,60 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 
 /**
  *
  * @author adrianadewunmi
  */
 public class ValidateAd extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ValidateAd</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ValidateAd at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -68,17 +23,47 @@ public class ValidateAd extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        PrintWriter printWriterOut = response.getWriter();
+        
+        // Fetch username and password
+        String userName = request.getParameter("Ausername");
+        String userPassword = request.getParameter("Apassword");
+        
+        response.setContentType("text/html");
+        
+        // List of Admin usernames and password
+        String adminUserOne = "A101";
+        String adminUserOnePassword = "Admin101";
+        
+        String adminUserTwo = "A202";
+        String adminUserTwoPassword = "Admin202";
+        
+        String adminUserThree = "A303";
+        String adminUserThreePassword = "Admin303";
+        
+        String adminUserFour = "A404";
+        String adminUserFourPassword = "Admin404";
+        
+        // Check for valid username and password
+        if(userName.equals(adminUserOne) && userPassword.equals(adminUserOnePassword)){
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("AdminEvent.html");
+            requestDispatcher.forward(request, response);
+        }else if(userName.equals(adminUserTwo) && userPassword.equals(adminUserTwoPassword)){
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("AdminEvent.html");
+            requestDispatcher.forward(request, response);
+        }else if(userName.equals(adminUserThree) && userPassword.equals(adminUserThreePassword)){
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("AdminEvent.html");
+            requestDispatcher.forward(request, response);
+        }else if(userName.equals(adminUserFour) && userPassword.equals(adminUserFourPassword)){
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("AdminEvent.html");
+            requestDispatcher.forward(request, response);
+            // If username and password are invalid, enter else
+        }else{
+            printWriterOut.println("<center><h4>Please Enter Valid Username & Password for Admin!!!</center></h4>");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("Alogin.html");
+            requestDispatcher.include(request, response);
+        }
+        printWriterOut.close();
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
