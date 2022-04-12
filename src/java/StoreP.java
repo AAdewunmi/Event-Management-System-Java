a7c832d3022ef3d009b8a20ca9ed1b9153f36b68
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-04-12 08:40:55 +0100

    Twentieth Commit: Implement StoreP.java

--- src/java/StoreP.java
+++ src/java/StoreP.java
@@ -3,60 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 /**
  *
  * @author adrianadewunmi
  */
 public class StoreP extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet StoreP</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet StoreP at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -68,17 +29,43 @@ public class StoreP extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        String participantsName = request.getParameter("Pname");
+        String userName = request.getParameter("Pusername");
+        String userPassword = request.getParameter("Ppassword");
+        String confirmUserPassword = request.getParameter("Cpassword");
+        
+        // EventlyDB, name of table plogindetails
+        
+        if(userPassword.equals(confirmUserPassword)){
+            
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                String conURL = "jdbc:mysql:// localhost:3306/";
+                String dbname = "EventlyDB";
+                String dbusername = "root";
+                String dbuserpassword = "abc";
+                Connection con;
+                con = DriverManager.getConnection(conURL + dbname, dbusername, dbuserpassword);
+                Statement statement = con.createStatement();
+                String mysqlQuery = "insert into plogindetails values('"+userName+"','"+userPassword+"','"+participantsName+"')";
+                ResultSet resultSet = statement.executeQuery(mysqlQuery);
+                RequestDispatcher requestDispatcher;
+                requestDispatcher = request.getRequestDispatcher("Plogin.html");
+                requestDispatcher.forward(request, response);
+                con.close();
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        
+        }else{
+            out.println("<center><h1>!! Please Enter Password And Confirm Password Same !!</h1><center>");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("Psignup.html");
+            requestDispatcher.include(request, response);
+        }
+        
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
