a3dfb56ca15ae622dc756d1ece1c71a7009eb38d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-04-14 16:24:51 +0100

    Twenty-seventh Commit: Implement LoginDao.java

--- src/java/LoginDao.java
+++ src/java/LoginDao.java
@@ -1,3 +1,12 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,8 +18,21 @@
  */
 public class LoginDao {
     
-    public static boolean validate(String User_name, String Password){
-        return true;
+    public static boolean validate(String User_Name, String User_Password) throws SQLException{
+        boolean status = false;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection conURL = DriverManager.getConnection("jdbc:mysql://localhost:3306/EventlyDB", "root", "abc")) {
+                PreparedStatement preparedStatement = conURL.prepareStatement("select * from plogindetails where User_Name=? and User_Password=?");
+                preparedStatement.setString(1, User_Name);
+                preparedStatement.setString(2, User_Password);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                status = resultSet.next();
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return status;
     }
     
 }
--- src/java/VaPa.java
+++ src/java/VaPa.java
@@ -6,6 +6,9 @@
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,7 +18,7 @@ public class VaPa extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP <code>GET</code> method.
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -23,22 +26,26 @@ public class VaPa extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        String userName = request.getParameter("Pausername");
-        String userPassword = request.getParameter("Papassword");
-        
-        if(LoginDao.validate(userName, userPassword)){
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("ParticipantEvent.html");
-            requestDispatcher.forward(request, response);
-        }else{
-            out.print("<center><h1>Sorry username and password incorrect</h1></center>");
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("Plogin.html");
-             requestDispatcher.include(request, response);
+        try (PrintWriter out = response.getWriter()) {
+            String userName = request.getParameter("Pausername");
+            String userPassword = request.getParameter("Papassword");
+            
+            try {
+                if(LoginDao.validate(userName, userPassword)){
+                    RequestDispatcher requestDispatcher = request.getRequestDispatcher("ParticipantEvent.html");
+                    requestDispatcher.forward(request, response);
+                }else{
+                    out.print("<center><h1>Sorry User Name and Password Incorrect</h1></center>");
+                    RequestDispatcher requestDispatcher = request.getRequestDispatcher("Plogin.html");
+                    requestDispatcher.include(request, response);
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(VaPa.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        out.close();
     }
 
 }
