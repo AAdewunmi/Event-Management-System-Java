b85ee75769c24644b176cd453cdd0dbdcbd9298a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-04-14 15:05:29 +0100

    Twenty-sixth Commit: Create LoginDao.java

---
+++ src/java/LoginDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author adrianadewunmi
+ */
+public class LoginDao {
+    
+    public static boolean validate(String User_name, String Password){
+        return true;
+    }
+    
+}
