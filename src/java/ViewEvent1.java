02cbc30af39260ae62b7939609a6f4d4201c07d0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-04-20 16:29:32 +0100

    Thirty-Eighth Commit: Create ViewEvent1.java

---
+++ src/java/ViewEvent1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+/**
+ *
+ * @author adrianadewunmi
+ */
+public class ViewEvent1 extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+}
